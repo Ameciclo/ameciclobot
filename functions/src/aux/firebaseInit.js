4baf6a405d638876cbe1726a177c2f44df490dc6
dvalenca <dvalenca@gmail.com>
2024-04-24 12:28:17 -0300

    aux folder

---
+++ functions/src/aux/firebaseInit.js
@@ -0,0 +1,11 @@
+"use strict";
+require("dotenv").config({ path: "../../.env" });
+const admin = require("firebase-admin");
+const serviceAccount = require("../../credentials/firebase_credentials.json"); // Certifique-se que este caminho está correto
+admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount),
+    databaseURL: `https://${process.env.FIREBASE_PROJECT_ID}.firebaseio.com`,
+});
+const db = admin.database();
+module.exports = { admin, db };
+//# sourceMappingURL=firebaseInit.js.map
