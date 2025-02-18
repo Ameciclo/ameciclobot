99a745eac8b771446dfb8f8f0648a5906d0a2335
Daniel Valença <dvalenca@gmail.com>
2025-02-17 22:11:27 -0300

    novo comando /planilha

---
+++ functions/src/commands/apresentacao.ts
@@ -0,0 +1,14 @@
+
+  
+  export function getApresentacaoCommandName() {
+    return "/apresentacao";
+  }
+  
+  export function getApresentacaoCommandHelp() {
+    return "Use o comando `/apresentacao` para criar uma Google Presentation. O formato esperado é:\n\n`/apresentacao [título da apresentação]`";
+  }
+  
+  export function getApresentacaoCommandDescription() {
+    return "🎞️ Criar uma Google Presentation para apresentações.";
+  }
+  
---
+++ functions/src/commands/formulario.ts
@@ -0,0 +1,12 @@
+
+  export function getFormularioCommandName() {
+    return "/formulario";
+  }
+  
+  export function getFormularioCommandHelp() {
+    return "Use o comando `/formulario` para criar um Google Forms. O formato esperado é:\n\n`/formulario [título do formulário]`";
+  }
+  
+  export function getFormularioCommandDescription() {
+    return "📝 Criar um Google Forms para formulários.";
+  }
